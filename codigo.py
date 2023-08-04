68f47adc5a508ce5b8fbcbff0cb1d1bbf4479e6e
Laura <lau.lozano456@gmail.com>
2023-08-04 18:43:35 -0500

    actualizacion

--- codigo.py
+++ codigo.py
@@ -11,10 +11,13 @@ df1 = pd.DataFrame(rows)
 
 # Change to NaT the data that is not in yyyy-mm-dd format
 df1['release_date'] = pd.to_datetime(df1['release_date'], format='%Y-%m-%d', errors='coerce')
+
 #Do a filter in release date to drop NaN
 dff = df1.dropna(subset= ['release_date'])
+
 #Change to datetime the release date
 dff['release_date'] = pd.to_datetime(dff['release_date'])
+
 #Unnest the colum genres in the dataframe
 df_anid = dff.explode('genres')
 
@@ -22,8 +25,10 @@ df_anid = dff.explode('genres')
 
 def get_genero(year: int):
     df = df_anid[['release_date','genres']]
+    
 #Convert the time to datetime object.Then it converts the datetime object to period object with a yearly frequency
     years = pd.to_datetime(year,format = '%Y').to_period('Y')
+    
 #Filter the selectiong the rows only when the period is equal to the year stored in the variable
     df_filter = df[df['release_date'].dt.to_period('Y') == years]
     df_top = df_filter['genres'].value_counts()
@@ -37,8 +42,10 @@ def get_juegos(year: int):
     df = dff[['release_date','app_name']]
 #Convert the time to datetime object.Then it converts the datetime object to period object with a yearly frequency
     years = pd.to_datetime(year,format = '%Y').to_period('Y')
+    
 #Filter the selectiong the rows only when the period is equal to the year stored in the variable
     df_filter = df[df['release_date'].dt.to_period('Y') == years]
+    
 #Converted the column into a list,transform to string and contain it in the variable
     juegos_lanzados = df_filter['app_name'].astype(str).tolist()
     juegos_dict = {juego for juego in juegos_lanzados}
@@ -69,9 +76,11 @@ def get_earlyaccess(year: int):
 
 def get_sentiment( year : int):
     dfs = dff[['sentiment','release_date']]
+    
     #Use isin to created a boolean series that indicate the value of the column
     #The ~ operator is used to invert the boolean series. 
     mask = ~dfs['sentiment'].isin(['Overwhelmingly Positive','Mostly Positive','Very Positive','Positive', 'Mixed', 'Negative','Mostly Negative','Very Negative','Overwhelmingly Negative'])
+    
     #Select the rows where the mask is True and set that values in the column for None
     dfs.loc[mask, 'sentiment'] = 'None'
     years = pd.to_datetime(year,format = '%Y').to_period('Y')
@@ -81,3 +90,19 @@ def get_sentiment( year : int):
     return {year: num_critics.to_dict()}
 
 #print(sentiment(2014))
+
+import pickle
+
+def get_predict(year, early_access, sentiment):
+    
+    # Load the saved model from a file
+    with open('model_and_rmse.pkl', 'rb') as file:
+        model = pickle.load(file)
+    
+    # Create input data for prediction
+    X = [[year, early_access, sentiment]]
+    
+    # Make prediction
+    y_pred = model.predict(X)
+    
+    return y_pred
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from codigo import get_genero,get_juegos,get_specs,get_earlyaccess,get_sentiment
+from codigo import get_genero,get_juegos,get_specs,get_earlyaccess,get_sentiment,get_predict
 import pandas as pd
 from fastapi import FastAPI,HTTPException
 from fastapi.responses import JSONResponse
@@ -53,3 +53,27 @@ async def sentiment(year: int):
         return JSONResponse(content=json_compatible_item_data)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+from fastapi import FastAPI, HTTPException, Query
+
+app = FastAPI()
+
+@app.get("/Model", tags=['games'])
+async def model(
+    year,
+    early_access: int = Query(..., ge=0, le=1),
+    sentiment: int = Query(..., ge=1, le=9)
+):
+    # Validate input values
+    if not 1 <= sentiment <= 9:
+        raise HTTPException(status_code=400, detail="Sentiment must be between 1 and 9")
+    if not 0 <= early_access <= 1:
+        raise HTTPException(status_code=400, detail="Early access must be either 0 or 1")
+    
+    try:
+        result = get_predict(year, early_access, sentiment)
+        json_compatible_item_data = jsonable_encoder(result)
+        return JSONResponse(content=json_compatible_item_data)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
